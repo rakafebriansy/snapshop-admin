37d690fb69aa03bd2618d0d43335627ce8036502
rakafebriansy <kerenr445@gmail.com>
2025-02-12 09:25:58 +0700

    feat: Nav component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Nav from "../components/Nav";
 
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
@@ -13,8 +14,11 @@ import { Geist, Geist_Mono } from "next/font/google";
 
 export default function Home() {
   return (
-    <div className="">
-      Test
+    <div className="bg-green-900 w-screen h-screen flex">
+      <Nav />
+      <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+
+      </div>
     </div>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from '../../components/Nav';
 
 const LoginPage: React.FC = ({ }) => {
     const { data: session } = useSession();
 
     if (!session) {
         return (
-            <div className="bg-blue-900 w-screen h-screen flex items-center">
-                <div className="text-center w-full">
-                    <button onClick={() => signIn('google')} className='button-filled'>Login with Google</button>
-                </div>
+            <div className="text-center w-full">
+                <button onClick={() => signIn('google')} className='button-filled'>Login with Google</button>
             </div>
         );
     }
 
     return (
-        <div className="">logged in {session.user?.email}</div>
+        <div className="bg-green-900 w-screen h-screen flex items-center">
+            <div className="">logged in {session.user?.email}</div>
+        </div>
     );
 }
 export default LoginPage;
