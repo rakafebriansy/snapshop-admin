079af8d19b8ebb02473917d1d1ca3ca1e86edfad
rakafebriansy <kerenr445@gmail.com>
2025-02-24 12:02:28 +0700

    fix: error handling while invalid credential

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
 import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import client from "../../../lib/mongodb"
+import { JWT } from "next-auth/jwt"
+import NextAuth, { Account, Profile, Session } from "next-auth"
+import { adminEmails } from "../../../config/credentials"
 
 export const authOptions = {
   providers: [
@@ -11,6 +13,22 @@ export const authOptions = {
     }),
   ],
   adapter: MongoDBAdapter(client),
+  callbacks: {
+    async signIn({ user }: { user: any }) {
+      if (!user.email || !adminEmails.includes(user.email)) {
+        console.error(`User not authorized: ${user.email}`);
+        return false;
+      }
+      return true;
+    },
+    async session({ session, token, user }: {
+      session: Session,
+      token: JWT,
+      user: any
+    }) {
+      return session;
+    }
+  }
 }
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
